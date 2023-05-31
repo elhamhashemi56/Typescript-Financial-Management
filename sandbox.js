8c38bd0cad09eb50974b73e69a4f90e81d1013e2
unknown <elhamhashemi56@yahoo.com>
2023-05-31 12:35:25 +0200

    compile tsc with -w watch

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var chara = "elham";
+console.log("name", chara);
+
+var inputs = document.querySelectorAll("input");
+console.log("inputs", inputs);
+
+inputs.forEach(function (item) {
+    console.log("item",item);
+});
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+const chara="elham"
+console.log("name",chara);
+
+const inputs=document.querySelectorAll("input")
+console.log("inputs",inputs);
+
+inputs.forEach(item=>{
+    console.log("item",item);
+    
+})
